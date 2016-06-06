fad11defb2d006eab67f6348e4dc35c1387b4cf7
Richard Gomes <rgomes.info@gmail.com>
2016-06-06 10:40:04 +0100

    Bump bnd version to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object Library {
 
   // Versions
-  val bndVersion = "2.4.0"
+  val bndVersion = "3.2.0"
   val specs2Version = "1.14"
 
   // Libraries
-  val bndLib = "biz.aQute.bnd" % "bndlib" % bndVersion
+  val bndLib = "biz.aQute.bnd" % "biz.aQute.bndlib" % bndVersion
   val specs2 = "org.specs2" %% "specs2" % specs2Version
 }
 
